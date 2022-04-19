56d82d1ff91f76ec4c6bd33bff195b41193d69da
HP <HP@LAPTOP-946K1CLI>
2022-04-19 12:41:51 +0530

    First Commit

---
+++ Java_Programs/src/nandu/clob.java
@@ -0,0 +1,13 @@
+package nandu;
+
+public class clob {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=10;
+		System.out.println("sum of 2 no's"+(a+b));
+
+	}
+
+}
