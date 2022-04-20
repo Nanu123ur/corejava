5f70084f28604155598132fb3dd1dc2a5f6d8906
HP <HP@LAPTOP-946K1CLI>
2022-04-20 15:24:26 +0530

    First commit

---
+++ Java_Programs/src/nandu/Day1.java
@@ -0,0 +1,40 @@
+package nandu;
+
+public class Day1 {
+	
+	
+		int year_ofpurchase;
+		String make;
+		int model;
+		double cost;
+		public void display1()
+		{
+			System.out.println("year of purchase "+2001);
+			System.out.println("make"+"suzuki");
+			
+			
+		}
+		public void display2()
+		{
+			System.out.println("model "+1999);
+			System.out.println("cost"+20000);
+			
+		}
+		
+
+		public static void main(String[] args) 
+		{
+			
+			Day1 ob1=new Day1();
+			ob1.display1();
+			ob1.display2();
+			
+			
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+
+	
